897d1ce61c0a0dd7f51b901e81d00790236cf50d
bboudra <benboudra@gmail.com>
2016-03-15 14:38:27 -0600

    Create Test Classes. Refractor FileList Class To be FileContent Class and place it inside fileContent Package

---
+++ app/src/main/java/ninjapiratestudios/trackercamera/FileContent/FileContent.java
@@ -0,0 +1,7 @@
+package ninjapiratestudios.trackercamera.FileContent;
+
+/**
+ * Created by benjamin on 3/15/16.
+ */
+public class FileContent {
+}
---
+++ app/src/test/java/ninjapiratestudios/trackercamera/fileSystemTests/ItemFragmentTest.java
@@ -0,0 +1,28 @@
+package ninjapiratestudios.trackercamera.fileSystemTests;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import static org.mockito.Mockito.times;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyBoolean;
+import static org.mockito.Matchers.anyInt;
+
+/**
+ * Created by benjamin on 3/15/16.
+ */
+public class ItemFragmentTest {
+
+}
