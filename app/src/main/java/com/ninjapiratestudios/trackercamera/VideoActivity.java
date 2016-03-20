a4c52a68db1601d21ad8e921b5fb3d38e2709d65
bboudra <benboudra@gmail.com>
2016-03-20 16:48:16 -0600

    Removed floating Email Icon.

--- app/src/main/java/com/ninjapiratestudios/trackercamera/VideoActivity.java
+++ app/src/main/java/com/ninjapiratestudios/trackercamera/VideoActivity.java
@@ -22,15 +22,6 @@ public class VideoActivity extends AppCompatActivity implements ItemFragment.OnL
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        assert fab != null;
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
